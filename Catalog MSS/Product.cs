d866b4a2a3d83355ab7768c537ce77a2690111d7
HOME-PC\MYKYTA <nkologhrivko@gmail.com>
2023-07-26 20:57:36 +0300

    fished homework

---
+++ Catalog MSS/Product.cs
@@ -0,0 +1,10 @@
+namespace Catalog_MSS
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public double Price { get; set; }
+        public string? Description { get; set; }
+    }
+}
